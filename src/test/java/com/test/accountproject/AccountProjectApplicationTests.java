02722259d7b76335ffd94bb84cd10b975456747b
a-lahmad@expedia.com <a-lahmad@expedia.com>
2018-11-25 12:45:01 +0000

    Created end to end test for account application

--- src/test/java/com/test/accountproject/AccountProjectApplicationTests.java
+++ src/test/java/com/test/accountproject/AccountProjectApplicationTests.java
@@ -1,16 +1,31 @@
 package com.test.accountproject;
 
+import java.util.Collection;
+
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AccountProjectApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@Autowired
+	TestRestTemplate testRestTemplate;
+
+    @Test
+    public void should_return_collection_of_accounts() {
+        // Act
+        ResponseEntity<Collection> response = testRestTemplate.getForEntity("/account/rest/json", Collection.class);
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        Assertions.assertThat(response.getBody().size() > 0);
+    }
 
 }
