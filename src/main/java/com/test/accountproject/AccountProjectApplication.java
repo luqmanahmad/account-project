1080a106cb6ec2fadc3f1884805622fa6df5ea2b
a-lahmad@expedia.com <a-lahmad@expedia.com>
2018-11-25 12:36:20 +0000

    Initial project structure

---
+++ src/main/java/com/test/accountproject/AccountProjectApplication.java
@@ -0,0 +1,12 @@
+package com.test.accountproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountProjectApplication.class, args);
+	}
+}
