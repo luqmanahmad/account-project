cf9002de9be18c44670184ed8fe2c9be9bc76db3
a-lahmad@expedia.com <a-lahmad@expedia.com>
2018-11-25 22:17:19 +0000

    Added controller implementation for accounts

--- src/main/java/com/test/accountproject/controller/AccountController.java
+++ src/main/java/com/test/accountproject/controller/AccountController.java
@@ -1,4 +1,17 @@
 package com.test.accountproject.controller;
 
+import java.util.Collection;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.test.accountproject.domain.Account;
+
+@RestController
 public class AccountController {
+
+    @GetMapping("/account/rest/json")
+    public Collection<Account> getAccounts() {
+        return null;
+    }
 }
---
+++ src/main/java/com/test/accountproject/domain/Account.java
@@ -0,0 +1,4 @@
+package com.test.accountproject.domain;
+
+public class Account {
+}
