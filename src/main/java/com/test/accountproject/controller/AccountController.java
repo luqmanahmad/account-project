7772effb96c9d6176158d524c602a63a09f20df3
a-lahmad@expedia.com <a-lahmad@expedia.com>
2018-11-25 23:26:57 +0000

    Added rest end point for create new account

--- src/main/java/com/test/accountproject/controller/AccountController.java
+++ src/main/java/com/test/accountproject/controller/AccountController.java
@@ -2,7 +2,11 @@ package com.test.accountproject.controller;
 
 import java.util.Collection;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.test.accountproject.domain.Account;
@@ -20,5 +24,10 @@ public class AccountController {
     @GetMapping("/rest/account/json")
     public Collection<Account> getAccounts() {
         return accountService.getAccounts();
+    }
+
+    @PostMapping(value = "/rest/account/json", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<String> createAccount(@RequestBody Account account) {
+        return ResponseEntity.ok().build();
     }
 }
