789f4d4b9c99efd4113990e9bb209fb8c546a667
a-lahmad@expedia.com <a-lahmad@expedia.com>
2018-11-26 00:06:52 +0000

    Added controller for delete entity

--- src/main/java/com/test/accountproject/controller/AccountController.java
+++ src/main/java/com/test/accountproject/controller/AccountController.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +33,10 @@ public class AccountController {
         accountService.save(account);
 
         return ResponseEntity.ok("Account has been successfully added.");
+    }
+
+    @DeleteMapping("/rest/account/json/{id}")
+    public ResponseEntity<String> deleteAccount(@PathVariable long id) {
+        return ResponseEntity.ok().build();
     }
 }
